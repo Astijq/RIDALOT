88acc011a0b3b2c617f4038fb93488b29e8971bc
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-09-13 18:10:22 +0300

    making a commit on VS

--- BlazorApp2/BlazorApp2/Program.cs
+++ BlazorApp2/BlazorApp2/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. //Im making a change
+// Configure the HTTP request pipeline. //Im making a change(Astijus)
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");

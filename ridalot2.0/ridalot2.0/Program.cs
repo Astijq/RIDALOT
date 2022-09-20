9c16a1a8e425974ca5a08e01bb75f537af8cfbe1
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-09-20 17:32:25 +0300

    testing git ignore

--- ridalot2.0/ridalot2.0/Program.cs
+++ ridalot2.0/ridalot2.0/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. / /asdasikdiasfuasfi
+// Configure the HTTP request pipeline. / /asdasikdiasfuasfi //kas skaitys tas gaidys
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");

eabb3f8bee0e9d2e176f91a1247a9a69e33e045c
Neringa Maj <maj.neringa@gmail.com>
2022-09-20 21:48:26 +0300

    Add a tab to create an ad

---
+++ ridalot3.0/ridalot3.0/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ridalot3._0.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ ridalot3.0/ridalot3.0/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ridalot3._0.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}

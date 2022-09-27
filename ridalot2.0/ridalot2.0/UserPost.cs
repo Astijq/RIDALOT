d3b16eda572c5c43e9736225d1a8268198e804b3
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-09-27 22:12:34 +0300

    Testing reading from file

---
+++ ridalot2.0/ridalot2.0/UserPost.cs
@@ -0,0 +1,43 @@
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("459d6f82-996b-40b1-920d-da313af445f8")]
+
+namespace Pages
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

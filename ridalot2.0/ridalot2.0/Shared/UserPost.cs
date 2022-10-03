bf1453aea0ff7f5e8a84e07bf70661ef0d5cdd2d
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-10-03 16:58:20 +0300

    add Class that holds post info and logic for creating a list of posts

---
+++ ridalot2.0/ridalot2.0/Shared/UserPost.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Data;
+
+namespace ridalot2._0.Shared
+{
+    public class UserPost : ComponentBase
+    {
+        protected int width;
+        protected int height;
+        protected string description;
+        protected string img;
+        protected string date;
+        protected IEnumerable<UserPost> postList;
+        void createList(IEnumerable<string> data)
+        {
+            postList = data.Select(item => new UserPost
+            {
+                // width = ...;
+            })
+            .ToList();
+        }
+    }
+
+
+}

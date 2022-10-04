f7d1be4243d494ab316c1576865d06825f0bbfa7
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-10-04 17:19:06 +0300

    create Read from file class

---
+++ ridalot2.0/ridalot2.0/Data/ReadPost.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ridalot2._0.Data
+{
+    public class ReadPost
+    {
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("PostInfo.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Post> items = JsonConvert.DeserializeObject<List<Post>>(PostInfo.json);
+            }
+        }
+    }
+}

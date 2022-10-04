8ef27bf25c03e368a6f073627f3a442cb91f69c0
majne <majne@LAPTOP-M7H959U6>
2022-10-04 12:14:42 +0300

    a new class that defines Post and changes in CreateAnAd to write to json file

---
+++ ridalot2.0/ridalot2.0/Data/Post.cs
@@ -0,0 +1,12 @@
+﻿public class Post
+{
+    public string? User { get; set; }
+    public string? Image { get; set; }
+    public string? Description { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public int Length { get; set; }
+    public int Weight { get; set; }
+
+}
+

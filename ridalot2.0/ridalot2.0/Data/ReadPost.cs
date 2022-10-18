d8711e5c8736d6e0507bfa6b6363ee227dc72f4e
majne <majne@LAPTOP-M7H959U6>
2022-10-18 21:12:09 +0300

    added constant method

--- ridalot2.0/ridalot2.0/Data/ReadPost.cs
+++ ridalot2.0/ridalot2.0/Data/ReadPost.cs
@@ -4,9 +4,8 @@
     using System.Collections.Generic;
     using System.Collections;
 
-    public class ReadPost<T> : IEnumerable
+    public class ReadPost<T>
     {
-
         public IEnumerable<T>? posts { get; set; } = new List<T>();
         public void LoadJson(string file = "PostInfo.json")
         {
@@ -15,11 +14,6 @@
                 string json = r.ReadToEnd();
                 posts = JsonConvert.DeserializeObject<List<T>>(json);
             }
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            throw new NotImplementedException();
         }
     }
 }

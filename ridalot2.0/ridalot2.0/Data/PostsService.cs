f3700412f39be3b487f0c28f8381c99404d01724
Neringa <93888787+neringamaj@users.noreply.github.com>
2022-10-26 16:03:43 +0300

    add database and basic functions of it (#53)
    * add database and basic functions of it
    
    * all fixed
    
    * correct user and auth
    
    * fixes
    
    Co-authored-by: majne <majne@LAPTOP-M7H959U6>
    Co-authored-by: Astijus Kurlinskas <astijus.kurlinskas@gmail.com>

---
+++ ridalot2.0/ridalot2.0/Data/PostsService.cs
@@ -0,0 +1,90 @@
+﻿using ridalot2._0.Data.RIDALOT;
+using Microsoft.EntityFrameworkCore;
+
+namespace ridalot2._0.Data
+{
+    public class PostsService
+    {
+        private readonly RIDALOTContext _context;
+        public PostsService(RIDALOTContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Posts>>
+            GetAllPostsAsync()
+        {
+            return await _context.Posts
+                 .AsNoTracking().ToListAsync();
+        }
+        public async Task<List<Posts>>
+            GetMyPostsAsync(string strCurrentUser)
+        {
+            return await _context.Posts
+                 .Where(x => x.User == strCurrentUser)
+                 .AsNoTracking().ToListAsync();
+        }
+        public async Task<List<Posts>>
+            GetMyTasksAsync(string strCurrentUser)
+        {
+            return await _context.Posts
+                 .Where(x => x.Worker == strCurrentUser)
+                 .AsNoTracking().ToListAsync();
+        }
+        public async Task<List<Posts>>
+            GetFeedPostsAsync(int strCurrentUser)
+        {
+            return await _context.Posts
+                 .Where(x => x.Status == strCurrentUser)
+                 .AsNoTracking().ToListAsync();
+        }
+        public Task<Posts>
+            CreatePostAsync(Posts post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+            return Task.FromResult(post);
+        }
+
+        public Task<bool>
+           DeletePostAsync(Posts post)
+        {
+            var ExistingPost =
+                _context.Posts
+                .Where(x => x.Id == post.Id)
+                .FirstOrDefault();
+            if (ExistingPost != null)
+            {
+                _context.Posts.Remove(ExistingPost);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task<bool>
+            UpdatePostAsync(Posts post)
+        {
+            var ExistingPost =
+                _context.Posts
+                .Where(x => x.Id == post.Id)
+                .FirstOrDefault();
+            if (ExistingPost != null)
+            {
+                ExistingPost.Status =
+                    post.Status;
+                ExistingPost.Worker =
+                    post.Worker;
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+    }
+}

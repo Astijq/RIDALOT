4098d0701152aed42efefe5dc23d9e20e1203d80
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-11-15 23:23:58 +0200

    debug

--- ridalot2.0/RidalotTests/UnitTest1.cs
+++ ridalot2.0/RidalotTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using ridalot2._0.Data;
+using ridalot2._0.Pages;
 
 namespace RidalotTests
 {
@@ -11,6 +12,22 @@ namespace RidalotTests
             var test = p.CalculatePay(10.1f, 2);
             var expected = 9.5f;
             Assert.Equal(expected, test);
+        }
+        [Fact]
+        public void TestRegex()
+        {
+            var test = Constants.addressMatch("Didlaukio g. 47");
+            var expected = true;
+            Assert.Equal(expected, test);
+        }
+        [Fact]
+        public void TestDeletePhoto()
+        {
+            CreateAnAd ca = new CreateAnAd();
+            ca.imgUrls.Add("first");
+            ca.imgUrls.Add("second");
+            ca.imgUrls.Add("third");
+            //cannot access deletephoto
         }
     }
 }

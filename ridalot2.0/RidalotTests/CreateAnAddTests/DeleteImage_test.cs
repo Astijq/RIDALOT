a3031d798d47f8fe6d1abe283207cd35ef644a7d
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-11-22 14:26:36 +0200

    pathing

---
+++ ridalot2.0/RidalotTests/CreateAnAddTests/DeleteImage_test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using ridalot2._0.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RidalotTests.CreateAnAddTests
+{
+    public class DeleteImage_test
+    {
+        [Fact]
+        public void TwoPhotos_DeleteOne_OneLeft()
+        {
+            CreateAnAd ca = new CreateAnAd();
+            ca.imgUrls.Add("first");
+            ca.imgUrls.Add("second");
+
+            ca.DeletePhoto("second");
+            Assert.Single(ca.imgUrls);
+        }
+    }
+}

853ec0ab3e0afb96a242b1f428df53e1a9c3a7fc
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-11-22 19:02:41 +0200

    DBposts tests

---
+++ ridalot2.0/RidalotTests/DBServiceTests/DBInMemory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ridalot2._0.Data.RIDALOT;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+using ridalot2._0.Data;
+
+namespace RidalotTests.DBServiceTests
+{
+    public class DBInMemory : IDisposable
+    {
+        private readonly DbConnection _connection;
+        private readonly DbContextOptions<RIDALOTContext> _contextOptions;
+        public DBInMemory()
+        {
+            // Create and open a connection. This creates the SQLite in-memory database, which will persist until the connection is closed
+            // at the end of the test (see Dispose below).
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            // These options will be used by the context instances in this test suite, including the connection opened above.
+            _contextOptions = new DbContextOptionsBuilder<RIDALOTContext>()
+                .UseSqlite(_connection).Options;
+
+            // Create the schema and seed some data
+            using var context = new RIDALOTContext(_contextOptions);
+
+            if (context.Database.EnsureCreated())
+            {
+                using var viewCommand = context.Database.GetDbConnection().CreateCommand();
+                viewCommand.CommandText = @"
+CREATE VIEW AllResources AS
+SELECT Url
+FROM Blogs;"
+                ;
+                viewCommand.ExecuteNonQuery();
+            }
+            context.AddRange(
+                new Posts { Id = 1, Status = 0, User = "user1", Worker = "worker1" },
+                new Posts { Id = 2, Status = 1, User = "user2", Worker = "worker2" });
+            context.SaveChanges();
+        }
+
+        public RIDALOTContext CreateContext() => new RIDALOTContext(_contextOptions);
+
+        public void Dispose() => _connection.Dispose();
+
+    }
+}
+

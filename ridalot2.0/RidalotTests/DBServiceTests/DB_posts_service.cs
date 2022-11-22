6daf98ce97b62e185bc1f910668bba6f09f600a1
Astijus Kurlinskas <90764233+Astijq@users.noreply.github.com>
2022-11-22 20:47:07 +0200

    DBposts tests (#74)
    Co-authored-by: Neringa <93888787+neringamaj@users.noreply.github.com>

---
+++ ridalot2.0/RidalotTests/DBServiceTests/DB_posts_service.cs
@@ -0,0 +1,73 @@
+﻿using ridalot2._0.Data.RIDALOT;
+using ridalot2._0.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RidalotTests.DBServiceTests
+{
+    public class DB_posts_service : DBInMemory
+    {
+        [Fact]
+        public async Task test_GetAllPosts()
+        {
+            using var context = CreateContext();
+            DBService service = new DBService(context);
+            var posts = new List<Posts>();
+            posts = await service.GetAllPostsAsync();
+
+            Assert.Collection(posts,
+                post => Assert.Equal("user1", post.User),
+                post => Assert.Equal("user2", post.User)
+                );
+        }
+
+        [Fact]
+        public async Task test_GetMyPosts()
+        {
+            using var context = CreateContext();
+            DBService service = new DBService(context);
+            var posts = new List<Posts>();
+            posts = await service.GetMyPostsAsync("user1");
+
+            Assert.Collection(posts,
+                post => Assert.Equal("user1", post.User));
+        }
+
+        [Fact]
+        public async Task test_GetMyTasks()
+        {
+            using var context = CreateContext();
+            DBService service = new DBService(context);
+            var posts = new List<Posts>();
+            posts = await service.GetMyTasksAsync("worker2");
+
+            Assert.Collection(posts,
+                post => Assert.Equal("worker2", post.Worker));
+        }
+        [Fact]
+        public async Task test_GetFeedPosts()
+        {
+            using var context = CreateContext();
+            DBService service = new DBService(context);
+            var posts = new List<Posts>();
+            posts = await service.GetFeedPostsAsync();
+
+            Assert.Collection(posts,
+                post => Assert.Equal(0, post.Status));
+        }
+
+        [Fact]
+        public async Task test_AddPost()
+        {
+            using var context = CreateContext();
+            DBService service = new DBService(context);
+
+            var post = new Posts();
+
+            Assert.True(await service.CreatePostAsync(post))
+        }
+    }
+}
--- ridalot2.0/ridalot2.0/Data/DBService.cs
+++ ridalot2.0/ridalot2.0/Data/DBService.cs
@@ -6,53 +6,37 @@ namespace ridalot2._0.Data
     public class DBService
     {
         private Lazy<RIDALOTContext> _context;
-        public RIDALOTContext context
-        {
-            get
-            {
-                return _context.Value;
-            }
-        }
+        public RIDALOTContext context { get{return _context.Value;} }
 
         public DBService(RIDALOTContext context)
         {
             _context = new Lazy<RIDALOTContext>(() => context);
         }
-        public async Task<List<Posts>>
-            GetAllPostsAsync()
+
+        public async Task<List<Posts>> GetAllPostsAsync()
         {
             return await _context.Value.Posts
                  .AsNoTracking().ToListAsync();
         }
-        public async Task<List<Posts>>
-            GetMyPostsAsync(string strCurrentUser)
+        public async Task<List<Posts>> GetMyPostsAsync(string strCurrentUser)
         {
             return await _context.Value.Posts
                  .Where(x => x.User == strCurrentUser)
                  .AsNoTracking().ToListAsync();
         }
-        public async Task<List<Images>>
-            GetImagesAsync()
-        {
-            return await _context.Value.Images.Include(p => p.Posts)
-                 .AsNoTracking().ToListAsync();
-        }
-        public async Task<List<Posts>>
-            GetMyTasksAsync(string strCurrentUser)
+        public async Task<List<Posts>> GetMyTasksAsync(string strCurrentUser)
         {
             return await _context.Value.Posts
                  .Where(x => x.Worker == strCurrentUser)
                  .AsNoTracking().ToListAsync();
         }
-        public async Task<List<Posts>>
-            GetFeedPostsAsync(Status strCurrentUser)
+        public async Task<List<Posts>> GetFeedPostsAsync()
         {
             return await _context.Value.Posts
-                 .Where(x => x.Status == strCurrentUser)
+                 .Where(x => x.Status == 0)
                  .AsNoTracking().ToListAsync();
         }
-        public async Task<Posts>
-            CreatePostAsync(Posts post)
+        public async Task<Posts> CreatePostAsync(Posts post)
         {
             _context.Value.Posts.Add(post);
             _context.Value.SaveChanges();
@@ -66,9 +50,12 @@ namespace ridalot2._0.Data
             _context.Value.SaveChanges();
             return await Task.FromResult(worker);
         }
-
-        public async Task<Images>
-             CreateImageAsync(Images img)
+        public async Task<List<Images>> GetImagesAsync()
+        {
+            return await _context.Value.Images.Include(p => p.Posts)
+                 .AsNoTracking().ToListAsync();
+        }
+        public async Task<Images> CreateImageAsync(Images img)
         {
             _context.Value.Images.Add(img);
             _context.Value.SaveChanges();

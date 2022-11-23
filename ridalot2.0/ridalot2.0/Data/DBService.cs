fcc15f9695e2464df93bed4fdcbc72bff4110ab2
Astijus Kurlinskas <90764233+Astijq@users.noreply.github.com>
2022-11-23 15:53:18 +0200

    Db image tests (#83)
    * c
    
    * all service tests

--- ridalot2.0/ridalot2.0/Data/DBService.cs
+++ ridalot2.0/ridalot2.0/Data/DBService.cs
@@ -6,7 +6,7 @@ namespace ridalot2._0.Data
     public class DBService
     {
         private Lazy<RIDALOTContext> _context;
-        public RIDALOTContext context { get{return _context.Value;} }
+        public RIDALOTContext context { get {return _context.Value;} }
 
         public DBService(RIDALOTContext context)
         {
@@ -42,40 +42,6 @@ namespace ridalot2._0.Data
             _context.Value.SaveChanges();
             return Task.FromResult(true);
         }
-
-        public Task<bool>
-             CreateWorkerAsync(Workers worker)
-        {
-            _context.Value.Workers.Add(worker);
-            _context.Value.SaveChanges();
-            return Task.FromResult(true);
-        }
-
-        public Task<bool>
-             CreateImageAsync(Images img)
-        {
-            _context.Value.Images.Add(img);
-            _context.Value.SaveChanges();
-            return Task.FromResult(true);
-        }
-
-        public Task<bool> DeletePostAsync(Posts post)
-        {
-            var ExistingPost =
-                _context.Value.Posts
-                .Where(x => x.Id == post.Id)
-                .FirstOrDefault();
-            if (ExistingPost != null)
-            {
-                _context.Value.Posts.Remove(ExistingPost);
-                _context.Value.SaveChanges();
-            }
-            else
-            {
-                return Task.FromResult(false);
-            }
-            return Task.FromResult(true);
-        }
         public Task<bool> UpdatePostAsync(Posts post)
         {
             var ExistingPost =
@@ -94,10 +60,39 @@ namespace ridalot2._0.Data
             }
             return Task.FromResult(true);
         }
+        public Task<bool> DeletePostAsync(Posts post)
+        {
+            var ExistingPost =
+                _context.Value.Posts
+                .Where(x => x.Id == post.Id)
+                .FirstOrDefault();
+            if (ExistingPost != null)
+            {
+                _context.Value.Posts.Remove(ExistingPost);
+                _context.Value.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+        public Task<bool> CreateImageAsync(Images img)
+        {
+            _context.Value.Images.Add(img);
+            _context.Value.SaveChanges();
+            return Task.FromResult(true);
+        }
         public async Task<List<Images>> GetImagesAsync()
         {
             return await _context.Value.Images.Include(p => p.Posts)
                  .AsNoTracking().ToListAsync();
+        }
+        public Task<bool> CreateWorkerAsync(Workers worker)
+        {
+            _context.Value.Workers.Add(worker);
+            _context.Value.SaveChanges();
+            return Task.FromResult(true);
         }
         public async Task<List<Workers>> GetAllWorkersAsync()
         {

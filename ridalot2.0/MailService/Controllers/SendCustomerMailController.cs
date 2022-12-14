4aa8f25ba222fc87589047e768af44f8f973bdc2
Astijus Kurlinskas <astijus.kurlinskas@gmail.com>
2022-12-14 12:04:33 +0200

    implement mailing service and use it in the project

---
+++ ridalot2.0/MailService/Controllers/SendCustomerMailController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace MailService.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class SendCustomerMailController : ControllerBase
+	{
+		[HttpGet]
+		public string sendMail(string custEmail, string ownerEmail, string subject, string text, string key)
+		{
+			var apiKey = key;
+			var client = new SendGridClient(apiKey);
+			var msg = new SendGridMessage()
+			{
+				From = new EmailAddress(ownerEmail),
+				Subject = subject,
+				PlainTextContent = text
+			};
+			msg.AddTo(new EmailAddress(custEmail));
+			var response = client.SendEmailAsync(msg);
+			return "success";
+		}
+
+	}
+}
--- ridalot2.0/ridalot2.0/Pages/PageSupport/CustomerMail.cs
+++ ridalot2.0/ridalot2.0/Pages/PageSupport/CustomerMail.cs
@@ -1,18 +1,42 @@
-﻿using MailSendReference;
-using Microsoft.AspNetCore.Mvc;
-using ridalot2._0.Shared;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ridalot2._0.Pages.PageSupport
 {
-    public class CustomerMail : LoginControl
+    public class CustomerMail
     {
-        sendMailPostedRequest p = new sendMailPostedRequest();
-        public void send()
+
+        string custEmail = "astijusmc@gmail.com";
+        string ownerEmail = "ridalot.info@gmail.com";
+        string postCreatedSubject = "Post Created Successfully";
+        string postCreatedText = "Your post has been added, wait for someone to contact you about the pickup";
+        string? API
         {
-            p(Email, GivenName);
+            get
+            {
+                var builder = new ConfigurationBuilder().AddUserSecrets<CustomerMail>();
+                var configuration = builder.Build();
+                // Access the secrets using the configuration object
+                return configuration["sgAPI"];
+            }
         }
-            //Email, GivenName
+        public async void sendMail()
+        {
+
+            // Create an instance of the HttpClient class
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7027/");
+
+            var requestUrl = $"api/ApprovedPost?custEmail={custEmail}&ownerEmail={ownerEmail}" +
+                $"&subject={postCreatedSubject}&text={postCreatedText}&key={API}";
+
+            // Make a GET request to the Web API's endpoint
+            var response = await client.GetAsync(requestUrl);
+
+            // Read the response as a string
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+        }
     }
+
 }
